6a86b111da7128febd43306fb65793c51afe1365
maviz <cheema.maviz@gmail.com>
2019-02-23 18:04:22 +0500

    signin redirect added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -49,4 +49,10 @@ class ApplicationController < ActionController::Base
     redirect_to request.referer || root_path,
                 flash: { error: exception.message }
   end
+
+  protected
+  
+  def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+  end
 end
